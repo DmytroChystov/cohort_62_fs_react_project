fc9a22e1f2ca11cb2016670e4ff9c4a9766e71a6
gzavertyaev-dev <gzavertyaev.dev@gmail.com>
2025-11-06 22:10:44 +0100

    Add basic app architecture

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Lesson_02 from "./lessons/Lesson_02/Lesson_02";
+
+function App() {
+  return (
+    <>
+      <Lesson_02 />
+    </>
+  );
+}
+
+export default App;
---
+++ src/lessons/Lesson_02/Lesson_02.jsx
@@ -0,0 +1,9 @@
+function Lesson_02() {
+  return (
+    <div>
+      <p>Lesson 02</p>
+    </div>
+  );
+}
+
+export default Lesson_02;
