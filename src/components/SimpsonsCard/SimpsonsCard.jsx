54d57e4912e9964a95c20864cc751f4b199b7a29
DmytroChystov <ramsneg2@gmail.com>
2025-11-15 12:56:49 +0100

    first commit

--- src/components/SimpsonsCard/SimpsonsCard.jsx
+++ src/components/SimpsonsCard/SimpsonsCard.jsx
@@ -1,13 +1,6 @@
 import "./styles.css";
 
-// props - это возможность передавать данные из родительского компонента в дочерний компонет
-// props является обьектом
-// function SimpsonsCard(props) {
 function SimpsonsCard({ firstName, lastName, job, hobby, avatar }) {
-  // До деструктуризации props в аргументе SimpsonsCard
-  // console.log(props);
-  // const { firstName, lastName, job, hobby, avatar } = props;
-
   return (
     <div className="card">
       <img className="avatar" src={avatar} alt="User Avatar" />
@@ -24,6 +17,7 @@ function SimpsonsCard({ firstName, lastName, job, hobby, avatar }) {
       </p>
     </div>
   );
-}
+} 
 
 export default SimpsonsCard;
+
--- src/homeworks/Homework_02/Homework_02.jsx
+++ src/homeworks/Homework_02/Homework_02.jsx
@@ -1,15 +1,34 @@
-import SimpsonsCard from "../../components/SimpsonsCard/SimpsonsCard";
-
 import "./styles.css";
 
 function Homework_02() {
   return (
     <div className="homework02_page_wrapper">
-      <SimpsonsCard />
-      <SimpsonsCard />
-      <SimpsonsCard /> 
+      <SimpsonsCard
+        firstName="Homer"
+        lastName="Simpson" 
+        job="Программист" 
+        hobby="Еда и сон"
+        avatar="https://avatars.mds.yandex.net/i?id=0452d3168af7c9a8d1344b9ae35164be088b7f45-9233745-images-thumbs&n=1340"
+      />
+
+      <SimpsonsCard 
+        firstName="Homer"
+        lastName="Simpson"
+        job="QA-инженер" 
+        hobby="Пить пиво"
+        avatar="https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png"
+      />
+
+      <SimpsonsCard
+        firstName="Homer"
+        lastName="Simpson"
+        job="Frontend dev"
+        hobby="Смотреть сериалы"
+        avatar="https://tse3.mm.bing.net/th/id/OIP.Tt78xKbjLk2dbxi9VuyLkwHaHJ?cb=ucfimg2ucfimg=1&rs=1&pid=ImgDetMain&o=7&rm=3"
+      />
     </div>
   );
 }
 
 export default Homework_02;
+
